4820ad941098c8bea73b95acb4ec6c829c2c86b6
Harsh Krishna Mhatre <harshmhatre1122@gmail.com>
2026-02-20 15:45:39 +0530

    css update

--- script.js
+++ script.js
@@ -95,3 +95,20 @@ if (menuToggle && mobileNav) {
     });
 
 });
+
+const toggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+    toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
+});
